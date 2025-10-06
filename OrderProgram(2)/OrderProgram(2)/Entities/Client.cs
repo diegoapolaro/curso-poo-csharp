065688094e6b49eafae350182b79623752e1a827
diegoapolaro <spkf1v3.twitch@gmail.com>
2025-10-05 22:44:30 -0300

    OrderProgram(2) Exercise

---
+++ OrderProgram(2)/OrderProgram(2)/Entities/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+using System.Collections.Generic;
+using OrderProgram_2_.Entities.Enums;
+
+namespace OrderProgram_2_.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return "Client: "
+            + Name
+            + BirthDate.ToString("dd/MM/yyyy")
+            + ") - "
+            + Email;
+        }
+    }
+}
---
+++ OrderProgram(2)/OrderProgram(2)/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderProgram_2_.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3,
+    }
+}
